c03452dcf42def4d7ac007ef42d1a0fae14fd72a
ic21b042 <ic21b042@technikum-wien.at>
2023-01-26 18:07:25 +0100

    #1 base for project
    #1 base for project

---
+++ src/main/java/eu/fhtw/slm/SlmApplication.java
@@ -0,0 +1,13 @@
+package eu.fhtw.slm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmApplication.class, args);
+	}
+
+}
